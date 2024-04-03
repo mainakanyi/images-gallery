3ec09b1f7ba6b48df328a4c098c2f3e15e3a5c2b
mainakanyi <mainakanyi9@gmail.com>
2024-04-03 23:40:28 +0300

    Added text Logo

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Navbar, Container } from 'react-bootstrap';
+import { ReactComponent as Logo } from '../images/logo.svg';
 
 const navbarStyle = {
-  backgroundColor: 'lightblue',
+  backgroundColor: '#eeeeee',
 };
 
 const Header = ({ title }) => {
   return (
     <Container>
       <Navbar style={navbarStyle} variant="light">
-        <Navbar.Brand href="/">{title}</Navbar.Brand>
+        {/* <Navbar.Brand href="/">{title}</Navbar.Brand> */}
+        <Logo style={{ maxWidth: '12rem', maxHeight: '2rem' }} />
       </Navbar>
     </Container>
   );
