15ce546d0e6eda0ec2b09e9d98a0e98ed019f4fd
mainakanyi <mainakanyi9@gmail.com>
2024-04-08 18:47:02 +0300

    Docker compose for API

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -18,7 +18,7 @@ const Search = ({ word, setWord, handleSubmit }) => {
               </Col>
               <Col>
                 <Button variant="primary" type="submit">
-                  Search
+                  Search Test
                 </Button>
               </Col>
             </Form.Row>
