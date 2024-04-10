28d90b9d8b0c70764667c29a89db7ef486ee9780
mainakanyi <mainakanyi9@gmail.com>
2024-04-10 18:14:49 +0300

    Introduce axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Search from './components/Search.js';
 import ImageCard from './components/ImageCard.js';
 import Welcome from './components/Welcome.js';
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+import axios from 'axios';
 
 const API_URL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:5000';
 
@@ -15,20 +16,19 @@ const App = () => {
     'Content-Type': 'application/json',
   };
 
-  const handleSearchSubmit = (e) => {
+  const handleSearchSubmit = async (e) => {
     e.preventDefault();
-    fetch(`${API_URL}/new-image?query=${word}`, {
-      headers: headers,
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(images);
-        setImages([data, ...images]);
-        console.log(images);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+
+    // Now workig with axios
+    try {
+      const res = await axios.get(`${API_URL}/new-image?query=${word}`);
+      console.log(res.data);
+      // Adding image to the state
+      setImages([{ ...res.data, title: word }, ...images]);
+    } catch (error) {
+      console.log(error);
+    }
+    // Clearing search form
     setWord('');
   };
 
